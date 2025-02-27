70aaf40c74ee4d8f2b389de82e0fa299903a569f
Rahul Kumar <119404150+thenamerahulkr@users.noreply.github.com>
2025-02-27 21:06:27 +0530

    Delete farmify-backend-main directory

--- farmify-backend-main/src/api/user-cart/controllers/user-cart.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * user-cart controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::user-cart.user-cart');
